ffa1a2f1cb3059f1e014aef11fb508a90abd4521
DE-IGNIS <chauhansudarsh@gmail.com>
2026-03-11 07:11:49 +0530

    Added unit test for bench

--- src/tests/runner.ts
+++ src/tests/runner.ts
@@ -3,11 +3,11 @@ import * as index from "../index.js";
 let passed = 0;
 let failed = 0;
 
-type TestFuntion =  () => Promise<Object> ;
+type TestFuntion = () => Promise<Object>;
 
-const it = (desc: string, fn: TestFuntion): void => {
+const it = async (desc: string, fn: TestFuntion): Promise<void> => {
   try {
-    fn();
+    await fn();
     passed++;
     console.log("\x1b[32m%s\x1b[0m", `\u2714 ${desc}`);
   } catch (error) {
@@ -23,11 +23,15 @@ function assert(condition: boolean, message?: string): void {
   }
 }
 
-it("bench() should return valid results", async () => {
-  assert(index.bench("Add 3 numbers", ) == assert.iterations === 1000, "Iterations should be 1000");
-  assert(index.bench("Add 3 numbers", ) == assert.meanTime > 0, "Mean time greater than 0");
-  assert(index.bench("Add 3 numbers", ) == assert.opsPerSec > 0, "OpsPerSec greater > 0");
+await it("bench() should return valid results",  async () =>  {
+  const result = await index.bench("test", () => {
+        1+1
+  });
+  assert(result.iterations === 1000, "iterations should be 1000");
+  assert(result.meanTime > 0, "meanTime should be > 0");
+  assert(result.opsPerSec > 0, "opsPerSec should be > 0");
 });
+
 
 console.log("\n");
 console.log(`Passed:${passed}`);
